670f52121909fe8497ad157a14334dd9e9f35dfe
Nick Bottomley <nhbottomley@gmail.com>
2017-01-31 21:56:06 -0800

    Update simple-client.rb

--- example-clients/simple-client.rb
+++ example-clients/simple-client.rb
@@ -3,11 +3,12 @@ require "uri"
 require "open-uri"
 
 def snake(str)
-  str.gsub(/::/, '/').
-  gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-  gsub(/([a-z\d])([A-Z])/,'\1_\2').
-  tr("-", "_").
-  downcase
+  str
+    .gsub(/::/, '/')
+    .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+    .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+    .tr("-", "_")
+    .downcase
 end
 
 # this client doesn't implement parameter validation, though it can be derived
